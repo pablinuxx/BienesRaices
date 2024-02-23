4b21e490c3dd7b784adeef9d6921d40a47f35457
psarasua <p.sarasua@hotmail.com>
2024-02-22 22:44:14 -0300

    agrego variables de entorno

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-import express from 'express'
-import usuarioRoutes from "./routes/usuarioRoutes.js"
+import express from "express";
+import usuarioRoutes from "./routes/usuarioRoutes.js";
+import db from "./config/db.js";
+
 //Creo la app
-const app = express()
+const app = express();
 
-
+//Conexion a la BD
+try {
+  await db.authenticate();
+  console.log("Conexion correcta a la BD");
+} catch (error) {
+  console.log("error");
+}
 //Habilitar Pug
-app.set('view engine',"pug")
-app.set("views","./views")
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 //Carpeta Publica
-app.use(express.static('public'))
-
+app.use(express.static("public"));
 
 //Routing
-app.use("/auth",usuarioRoutes)
-
+app.use("/auth", usuarioRoutes);
 
 //Definir puerto y arrancar la App
-const port = 4000
+const port = 4000;
 
-app.listen(port,()=>{
-    console.log(`El servidor esta corriendo en el puerto: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`El servidor esta corriendo en el puerto: ${port}`);
+});
